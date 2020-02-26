30793510ce1252bd80f50421cef254baa7be53cd
Fernando Parada <fernandoparada18@protonmail.com>
2020-02-26 17:17:20 -0400

    [UPD] Mejoras del tablero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,135 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Tablero
+        <small>Panel de Control</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Tablero</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <!-- Small boxes (Stat box) -->
     <div class="row">
+        @if(auth()->user()->type == 'admin')
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>{{ $chief }}</h3>
 
+                    <p>Registros</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>{{ $approved }}</h3>
 
+                    <p>Registros Aprobados</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>{{ $patrols }}</h3>
+
+                    <p>Amigos</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>{{ $new }}</h3>
+
+                    <p>Nuevos Registros</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+        @endif
+        @if(auth()->user()->type == 'standar')
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3 style="font-size: 30px">{{ auth()->user()->chief->patrols->count() }}</h3>
+
+                    <p>Amigos</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            @if(auth()->user()->chief->approved)
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                <div class="inner">
+                    <h3 style="font-size: 30px">CORRECTO</h3>
+
+                    <p>Información Aprobada</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            @else
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                <div class="inner">
+                    <h3 style="font-size: 30px">PENDIENTE</h3>
+
+                    <p>Verificando Información</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            @endif
+            <!-- ./col -->
+        @endif
     </div>
-</div>
+    <!-- /.row -->
+</section>
+<!-- /.content -->
 @endsection
